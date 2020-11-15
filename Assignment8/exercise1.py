4a8a7025818e4d05ed22d9395e85a74d55185101
Suning Yao <41555918+fewwwww@users.noreply.github.com>
2020-11-15 19:13:49 +0800

    Add files via upload

---
+++ Assignment8/exercise1.py
@@ -0,0 +1,26 @@
+# PROGRAMMING ASSIGNMENT 08
+# Filename: 'exercise1.py'
+#
+# See instructions in pdf file
+#
+
+def main():
+    """The program does the following:
+    - it inserts all the content from file2.txt to the beginning of file1.txt
+    
+    Note: After execution of the your program, only file1.txt is updated, file2.txt does not change."""
+    #WRITE YOUR PROGRAM HERE
+    file1 = open('file1.txt', 'r')
+    file1_data = file1.read()
+    file1.close()
+    file2_r = open('file2.txt', 'r')
+    file2_data = file2_r.read()
+    file2_r.close()
+    file1_w = open('file1.txt', 'w')
+    file1_w.write(file2_data + file1_data)
+    file1_w.close()
+    
+
+#Call the main() function
+if __name__ == '__main__':
+    main()
---
+++ Assignment8/exercise2.py
@@ -0,0 +1,42 @@
+# PROGRAMMING ASSIGNMENT 08
+# Filename: 'exercise2.py'
+#
+# See instructions in pdf file
+#
+
+def main():
+    """The program does the following:
+    1) asks the user to input a filename (supposedly in the same folder as this program)
+	2) reads the numbers (type int) in the file (filename given by the user in previous step) and stores them into a list
+	3) prints the minimum, the maximum and the average of the numbers (2 decimal places for the average)
+    
+    Note: your program should be impossible to crash. Think about all the possibilities to make your program crash.
+    If one of the steps throws an exception, display a message to the user and your program should loop back to step 1."""
+    #WRITE YOUR PROGRAM HERE
+    while True:
+        filename = input('Filename:\n')
+        try:
+            file = open(filename,'r')
+        except:
+           print('File not found')
+        else:
+            data = file.read()
+            lst = data.split('\n')
+            lst_corrected = []
+            for i in range(len(lst)):
+                try:
+                    lst[i] = int(lst[i])
+                    lst_corrected.append(lst[i])
+                except:
+                    continue
+            try:
+                print('Minimum: {0}\nMaximum: {1}\nAverage: {2:.2f}'.format(min(lst_corrected),max(lst_corrected),sum(lst_corrected)/len(lst_corrected)))
+                break
+            except:
+                print('File is empty')
+                continue
+    
+
+#Call the main() function
+if __name__ == '__main__':
+    main()
